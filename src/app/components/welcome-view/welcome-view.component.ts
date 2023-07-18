7cec05d04090a9b88258cdb838d6c30df275b6ce
luhpelayo <luhpelayo@gmail.com>
2023-07-18 17:16:49 -0400

    LP/welcome

--- src/app/components/welcome-view/welcome-view.component.ts
+++ src/app/components/welcome-view/welcome-view.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome-view',
   template: `
-    <div class="container d-flex flex-column align-items-center justify-content-center welcome-container">
+    <div class="container d-flex flex-column align-items-center justify-content-center welcome-container"
+         [style.background-image]="'url(' + backgroundImageUrl + ')'"
+    >
       <h1 class="text-white">BIENVENIDO A FLOTAS PL</h1>
       <button class="btn btn-light btn-lg text-purple mt-3" (click)="goToHome()">Compra tu pasaje</button>
     </div>
@@ -11,9 +13,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-view.component.css']
 })
 export class WelcomeViewComponent {
+  backgroundImageUrl = 'assets/flota.png';  // Utiliza la ruta completa relativa al archivo TypeScript
+
   goToHome() {
     // Agrega aquí la lógica para navegar a la ruta /home
   }
 }
-
-
